cb7943f73dd0c3f3b9a55a95708873aa3cf8d2d5
lixk <1749498702@qq.com>
2017-11-07 16:40:00 +0800

    changed subscribe and unsubscribe method

--- src/topic.js
+++ src/topic.js
@@ -5,34 +5,34 @@ WebSocket.prototype.subscribe = function(topic, callback){
     var _this = this;
     _this.topics = _this.topics || {};
     _this.topics[topic] = callback;
-    _this.newTopicQueue = _this.newTopicQueue || []
-    _this.newTopicQueue.push(topic);  //将新订阅的主题加入队列
+    _this.topicQueue = _this.topicQueue || []
+    _this.topicQueue.push({"topic": topic, "action": "subscribe"});  // put this topic to the queue
+
     _this.onopen = function() {
         setInterval(function(){
-            while(_this.newTopicQueue.length > 0){
-                var topic = _this.newTopicQueue.shift();
-                _this.send(JSON.stringify({"topic": topic, "action": "subscribe"}));
+            while(_this.topicQueue.length > 0){
+                var data = _this.topicQueue.shift();  // take a topic from the queue
+                _this.send(JSON.stringify(data));
             }
         }, 100);
 
-    }
-
-    _this.onmessage = function(event){
-        var data = event.data;
-        try {
-            data = JSON.parse(data);
-            var callback = _this.topics[data.topic];
-            callback(data.data);
-        }catch(e){
-            console.error(data);
+        _this.onmessage = function(event){
+            var data = event.data;
+            try {
+                data = JSON.parse(data);
+                var callback = _this.topics[data.topic];
+                callback(data.data);
+            }catch(e){
+                console.error(data);
+            }
         }
-    }
 
-    _this.onclose = function(){
-        console.info('connection closed!');
-    }
-    _this.onerror = function(){
-        console.error('connection error!');
+        _this.onclose = function(){
+            console.info('connection closed!');
+        }
+        _this.onerror = function(){
+            console.error('connection error!');
+        }
     }
 }
 
@@ -42,7 +42,9 @@ WebSocket.prototype.subscribe = function(topic, callback){
 WebSocket.prototype.unsubscribe = function(topic){
     var _this = this;
     _this.topics = _this.topics || {};
+    // remove the topic
     delete _this.topics[topic];
-    _this.send(JSON.stringify({"topic": topic, "action": "unsubscribe"}));
+    _this.topicQueue = _this.topicQueue || []
+    _this.topicQueue.push({"topic": topic, "action": "unsubscribe"});  // put this topic to the queue
     
 }
